54e82d30c643d97d0dba3a825ab686cdf15256cc
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-07-10 09:57:03 -0500

    tested, works

--- trigger.ino/trigger.ino.ino
+++ trigger.ino/trigger.ino.ino
@@ -1,13 +1,13 @@
 #include <EEPROM.h>
 int SIGNAL_PIN = A0;
-int TRIGGER_PIN = D2;
+int TRIGGER_PIN = 2;
 //saved values dirs:
-int SIGNALL_DIR = 0;
-int SIGNALH_DIR = 1;
+int SIGNAL_LOW_DIR = 0;
+int SIGNAL_HIGH_DIR = 1;
 int PULSE_WIDTH_DIR = 2;
 
-int signalLow = 0;
-int signalHigh = 255;
+int signalLow = 48;
+int signalHigh = 55;
 int pulseWidth = 16000;
 
 bool signalIsHigh = true;
@@ -16,14 +16,13 @@ int signal = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("beginning");
+  Serial.println("starting");
   pinMode(SIGNAL_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
-  //EEPROM.write(0, 74);
 
   //read signal "borders"
-  signalLow = EEPROM.read(SIGNALL_DIR);
-  signalHigh = EEPROM.read(SIGNALH_DIR);
+  signalLow = EEPROM.read(SIGNAL_LOW_DIR);
+  signalHigh = EEPROM.read(SIGNAL_HIGH_DIR);
   pulseWidth = EEPROM.read(PULSE_WIDTH_DIR);
   
   digitalWrite(TRIGGER_PIN, 1);   //set to high, ready to trigger
@@ -35,6 +34,8 @@ void loop() {
   if (signalIsHigh){
     if (signal < signalLow){
       cameraTrigger(pulseWidth);
+      Serial.println("trigger");
+      Serial.println(signal);
       signalIsHigh = false;
     }
   } else{
@@ -42,7 +43,7 @@ void loop() {
       signalIsHigh = true;    //time to reset, ready for a new trigger
   }
 
-  //Serial.println(value1);
+  //Serial.println(signal);
 }
 
 void cameraTrigger(int pulseWidth){
