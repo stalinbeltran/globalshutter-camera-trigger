dfb42237aa94ef2775200bf6f3e30ae3910e1094
stalinbeltran <stalin.beltran2006@gmail.com>
2025-09-18 14:39:54 -0500

    pulse width measured

--- trigger.ino/trigger.ino.ino
+++ trigger.ino/trigger.ino.ino
@@ -1,11 +1,15 @@
 #include <EEPROM.h>
 int SIGNAL_PIN0 = A0;
-int SIGNAL_PIN1 = A1;
+//int SIGNAL_PIN1 = A1;
 int TRIGGER_PIN = 2;
 
+//statuses:
+const int RESET = 0;
+const int MOBILE_COMING = 1;
+int status = RESET;
 
 //signal "borders"
-int signalLow = 40;
+int signalLow = 46;
 int signalHigh = 50;
 int pulseWidth = 31;    //31 is the minimum pulse width that works
 
@@ -13,10 +17,9 @@ int signal0 = 0;    //sensor for moving mirror
 int signal1 = 0;    //sensor for static mirror
 int counter0 = 0;   //status triggered in the begining
 int counter1 = 0;   //status triggered in the begining
+int beginningPulse0 = 0;
+int widthPulse0 = 0;
 
-int inicio = millis();
-int final = 0;
-int elapsed = 0;
 int referencia = 0;
 int factor = 15;
 
@@ -24,7 +27,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(SIGNAL_PIN0, INPUT);
-  pinMode(SIGNAL_PIN1, INPUT);
+  //pinMode(SIGNAL_PIN1, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
 
   digitalWrite(TRIGGER_PIN, 1);   //set to low, ready to trigger the optoisolator
@@ -33,9 +36,9 @@ void setup() {
 void loop() {
   int previousElapsed = 0;
   signal0 = analogRead(SIGNAL_PIN0);
-  signal1 = analogRead(SIGNAL_PIN1);
-  processSignal(signal0, counter0);
-  processSignal(signal1, counter1);
+  //signal1 = analogRead(SIGNAL_PIN1);
+  processSignal(signal0, counter0, beginningPulse0, widthPulse0, status);
+  //processSignal(signal1, counter1);
 
 
 }
@@ -46,20 +49,33 @@ void cameraTrigger(int pulseWidth){
   digitalWrite(TRIGGER_PIN, 1);
 }
 
-void processSignal(int signal, int &counter){
+void processSignal(int signal, int &counter, int &beginningPulse, int &widthPulse, int &status){
+  int final, widthPulseActual = 0;
   if (signal > signalHigh){
     counter++;               //reset trigger
+    if (counter == 1){
+      widthPulseActual = micros() - beginningPulse;
+      if (widthPulseActual > widthPulse*2){
+        status = MOBILE_COMING;
+        printValue("widthPulseActual", widthPulseActual);
+        printValue("widthPulse", widthPulse);
+      } else{
+        status = RESET;
+      }
+      
+        widthPulse = widthPulseActual;
+    }
   }
   if (signal < signalLow){
-    if (counter > 0){        //ready to trigger
-      cameraTrigger(pulseWidth);
-      counter = 0;           //triggered
-      
-      final = millis();
-      elapsed = final - inicio;
-      inicio = final;
-      //printValue("elapsed", elapsed);
-
+    if (counter > 0){        //detected mobile arm
+      if (status == MOBILE_COMING){
+        cameraTrigger(pulseWidth);    //pulse mobile mirror
+        delay(35);                    //replace 35 by 1/4 T
+        cameraTrigger(pulseWidth);    //pulse fixed mirror (triggered entirely by time)
+        status = RESET;
+      }
+      counter = 0;           //trigger once only
+      beginningPulse = micros();    //start measuring the arm "width" when crossing the sensor
     }
   }
 }
@@ -70,4 +86,3 @@ void printValue(String name, int value){
     Serial.print(value);
     Serial.println(" ");
 }
-
