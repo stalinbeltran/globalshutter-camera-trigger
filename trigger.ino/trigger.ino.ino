62190d54a2c170adb15f87f70c271146213a8f40
stalinbeltran <stalin.beltran2006@gmail.com>
2025-09-19 13:03:32 -0500

    both pulses

--- trigger.ino/trigger.ino.ino
+++ trigger.ino/trigger.ino.ino
@@ -6,7 +6,7 @@ int TRIGGER_PIN = 2;
 const int RESET = 0;
 const int MOBILE_COMING = 1;
 const int FIXED_COMING = 2;
-const int FIXED_WAITING = 2;
+const int FIXED_WAITING = 3;
 int status = RESET;
 
 //signal "borders"
@@ -18,6 +18,7 @@ int signal0 = 0;    //sensor for moving mirror
 int counter0 = 0;   //status triggered in the begining
 unsigned long beginningPulse0 = micros();
 unsigned long periodBeginning = micros();
+unsigned long beginningFixedWaiting = micros();
 unsigned long halfPeriod = 0;
 unsigned long quarterPeriod = 0;
 unsigned long widthPulse0 = 9999;
@@ -35,12 +36,14 @@ void loop() {
   signal0 = analogRead(SIGNAL_PIN0);
   processSignal(signal0, counter0, beginningPulse0, widthPulse0, status);
   periodMeasurement(periodBeginning, beginningPulse0, halfPeriod, quarterPeriod);
+  fixedMirrorPulse(beginningFixedWaiting, quarterPeriod);
 }
 
 void periodMeasurement(unsigned long &periodBeginning, unsigned long &actualPulseBeginning, 
   unsigned long &halfPeriod, unsigned long &quarterPeriod){
   if(periodBeginning != actualPulseBeginning){
     halfPeriod = (unsigned long)actualPulseBeginning - (unsigned long)periodBeginning;
+    halfPeriod = halfPeriod/1000;     //microseconds to milliseconds
     periodBeginning = actualPulseBeginning;
     quarterPeriod = halfPeriod/2;
   }
@@ -49,13 +52,14 @@ void periodMeasurement(unsigned long &periodBeginning, unsigned long &actualPuls
 void fixedMirrorPulse(unsigned long &beginningFixedWaiting, unsigned long &quarterPeriod){
   unsigned long final, dif;
   if (status == FIXED_COMING){
+    
     beginningFixedWaiting = millis();
     status = FIXED_WAITING;
     return;
   }
   if (status == FIXED_WAITING){
     final = millis();
-    dif = final - beginningFixedWaiting
+    dif = (unsigned long)final - (unsigned long)beginningFixedWaiting;
     if(dif >= quarterPeriod){         //arm is out of the way
       cameraTrigger(pulseWidth);      //camera capture fixed mirror
       status = RESET;
@@ -79,11 +83,8 @@ void processSignal(int signal, int &counter, unsigned long &beginningPulse,
     if (counter == 1){
       microsActual = (unsigned long)micros();
       widthPulseActual = (unsigned long)microsActual - (unsigned long)beginningPulse;
-        printValue("widthPulseActual", (unsigned long)widthPulseActual);
       if (widthPulseActual > widthPulse*2){     //widthPulseActual should be longer if it has no mirror
         status = MOBILE_COMING;
-      } else{
-        status = RESET;
       }
       
       widthPulse = widthPulseActual;    //save sensed pulse width
@@ -92,8 +93,6 @@ void processSignal(int signal, int &counter, unsigned long &beginningPulse,
   if (signal < signalLow){		//sensed pulse is now low
     if (counter > 0){        	//previously was high, so mobile arm is now detected
       beginningPulse = (unsigned long)micros();    //start measuring the arm "width" when crossing the sensor
-      
-//        printValue("---beginningPulse", beginningPulse);
       if (status == MOBILE_COMING){		//detected mobile mirror
         cameraTrigger(pulseWidth);    //pulse to camera capture mobile mirror
         status = FIXED_COMING;          //ready to read fixed mirror
