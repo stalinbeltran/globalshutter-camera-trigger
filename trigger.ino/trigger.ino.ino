c220b227af3d396a2a81a9b9d8edc6070f8d7dfc
stalinbeltran <stalin.beltran2006@gmail.com>
2025-09-19 10:16:57 -0500

    works (unsigned long issue)

--- trigger.ino/trigger.ino.ino
+++ trigger.ino/trigger.ino.ino
@@ -9,13 +9,13 @@ int status = RESET;
 
 //signal "borders"
 int signalLow = 46;
-int signalHigh = 50;
+int signalHigh = 52;
 int pulseWidth = 31;    //31 is the minimum pulse width that works
 
 int signal0 = 0;    //sensor for moving mirror
 int counter0 = 0;   //status triggered in the begining
-int beginningPulse0 = micros();
-int widthPulse0 = 999999999;
+unsigned long beginningPulse0 = micros();
+unsigned long widthPulse0 = 9999;
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,38 +37,44 @@ void cameraTrigger(int pulseWidth){
   digitalWrite(TRIGGER_PIN, 1);
 }
 
-void processSignal(int signal, int &counter, int &beginningPulse, int &widthPulse, int &status){
-  int widthPulseActual = 0;
+void processSignal(int signal, int &counter, unsigned long &beginningPulse, unsigned long &widthPulse, int &status){
+  unsigned long widthPulseActual = 0, microsActual = 0;
   if (signal > signalHigh){
     counter++;               //sensed pulse is now high
     if (counter == 1){
-      widthPulseActual = micros() - beginningPulse;
+      microsActual = (unsigned long)micros();
+      widthPulseActual = (unsigned long)microsActual - (unsigned long)beginningPulse;
+//        printValue("widthPulse", widthPulse);
+//        printValue("microsActual", (unsigned long)microsActual);
+//        printValue("beginningPulse", (unsigned long)beginningPulse);
+        printValue("widthPulseActual", (unsigned long)widthPulseActual);
       if (widthPulseActual > widthPulse*2){     //widthPulseActual should be longer
         status = MOBILE_COMING;
-//        printValue("widthPulseActual", widthPulseActual);
-//        printValue("widthPulse", widthPulse);
       } else{
         status = RESET;
       }
       
       widthPulse = widthPulseActual;    //save sensed pulse width
     }
+    return;
   }
   if (signal < signalLow){		//sensed pulse is now low
-    if (counter > 0){        	//detected mobile arm
+    if (counter > 0){        	//previously was high, so mobile arm is now detected
+      beginningPulse = (unsigned long)micros();    //start measuring the arm "width" when crossing the sensor
+      
+//        printValue("---beginningPulse", beginningPulse);
       if (status == MOBILE_COMING){		//detected mobile mirror
         cameraTrigger(pulseWidth);    //pulse mobile mirror
-        delay(35);                    //replace 35 by 1/4 T
-        cameraTrigger(pulseWidth);    //pulse fixed mirror (triggered entirely by time)
+        //delay(35);                    //replace 35 by 1/4 T
+        //cameraTrigger(pulseWidth);    //pulse fixed mirror (triggered entirely by time)
         status = RESET;
       }
       counter = 0;           //trigger once only
-      beginningPulse = micros();    //start measuring the arm "width" when crossing the sensor
     }
   }
 }
 
-void printValue(String name, int value){
+void printValue(String name, unsigned long value){
     Serial.print(" " + name + ":");
     Serial.print(value);
     Serial.println(" ");
