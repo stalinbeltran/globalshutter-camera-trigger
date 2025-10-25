d0b4ae152f160642016c807ba397922539fceed7
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-10-25 06:55:53 -0500

    .

--- trigger.ino/trigger.ino.ino
+++ trigger.ino/trigger.ino.ino
@@ -1,5 +1,5 @@
 #include <EEPROM.h>
-int SIGNAL_PIN0 = A0;
+int SIGNAL_MOBILE_MIRROR = A0;
 int SIGNAL_FIXED_MIRROR = A1;
 int TRIGGER_PIN = 2;
 
@@ -13,12 +13,12 @@ int status = RESET;
 //signal "borders"
 int signalLow = 46;
 int signalHigh = 52;
-int pulseWidth = 31;    //31 is the minimum pulse width that works
+const int PULSE_WIDTH = 31;    //31 is the minimum pulse width that works
 
 int signal0 = 0;    //sensor for moving mirror
 int signalFixedMirror = 0;    //sensor for fixed mirror
 int counter0 = 0;   //status triggered in the begining
-unsigned long beginningPulse0 = micros();
+unsigned long beginningMobileMirrorPulse = micros();
 unsigned long periodBeginning = micros();
 unsigned long beginningFixedWaiting = micros();
 unsigned long halfPeriod = 0;
@@ -28,7 +28,7 @@ unsigned long widthPulse0 = 9999;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(SIGNAL_PIN0, INPUT);
+  pinMode(SIGNAL_MOBILE_MIRROR, INPUT);
   pinMode(SIGNAL_FIXED_MIRROR, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
 
@@ -36,10 +36,10 @@ void setup() {
 }
 
 void loop() {
-  signal0 = analogRead(SIGNAL_PIN0);
+  signal0 = analogRead(SIGNAL_MOBILE_MIRROR);
   signalFixedMirror = analogRead(SIGNAL_FIXED_MIRROR);
-  processSignal(signal0, counter0, beginningPulse0, widthPulse0, status);
-  periodMeasurement(periodBeginning, beginningPulse0, halfPeriod, quarterPeriod);
+  processSignal(signal0, counter0, beginningMobileMirrorPulse, widthPulse0, status);
+  periodMeasurement(periodBeginning, beginningMobileMirrorPulse, halfPeriod, quarterPeriod);
   fixedMirrorPulse(beginningFixedWaiting, quarterPeriod, signalFixedMirror);
 }
 
@@ -64,7 +64,7 @@ void fixedMirrorPulse(unsigned long &beginningFixedWaiting, unsigned long &quart
     if (signalFixedMirror < signalLow - 6){
       //final = millis();
       //dif = (unsigned long)final - (unsigned long)beginningFixedWaiting;
-        cameraTrigger(pulseWidth);      //camera capture fixed mirror
+        cameraTrigger();      //camera capture fixed mirror
         status = RESET;
       //printValue("FixedMirror", dif);
     }
@@ -73,14 +73,14 @@ void fixedMirrorPulse(unsigned long &beginningFixedWaiting, unsigned long &quart
 }
 
 
-void cameraTrigger(int pulseWidth){
+void cameraTrigger(){
   digitalWrite(TRIGGER_PIN, 0);
-  delayMicroseconds(pulseWidth);
+  delayMicroseconds(PULSE_WIDTH);
   digitalWrite(TRIGGER_PIN, 1);
 }
 
 void processSignal(int signal, int &counter, unsigned long &beginningPulse, 
-  unsigned long &widthPulse, int &status){
+  unsigned long &widthPulse){
   unsigned long widthPulseActual = 0, microsActual = 0;
   if (signal > signalHigh){
     counter++;               //sensed pulse is now high
@@ -93,16 +93,18 @@ void processSignal(int signal, int &counter, unsigned long &beginningPulse,
       
       widthPulse = widthPulseActual;    //save sensed pulse width
     }
+	return
   }
   if (signal < signalLow){		//sensed pulse is now low
     if (counter > 0){        	//previously was high, so mobile arm is now detected
       beginningPulse = (unsigned long)micros();    //start measuring the arm "width" when crossing the sensor
       if (status == MOBILE_COMING){		//detected mobile mirror
-        cameraTrigger(pulseWidth);    //pulse to camera capture mobile mirror
+        cameraTrigger();    //pulse to camera capture mobile mirror
         status = FIXED_COMING;          //ready to read fixed mirror
       }
       counter = 0;           //trigger once only
     }
+	return
   }
 }
 
